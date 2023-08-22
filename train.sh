8ddb3d875133da69b949fcd91ba75190bf984eae
WillbeD0ne <linzihao2637@gmail.com>
2023-08-22 09:25:09 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -3,14 +3,14 @@ python train.py \
 --data_dir data/data/classification_dataset/images_resample \
 --train_anno_file label_all_nohardsample.txt \
 --batch-size 4 \
---model uniformer_small_IL \
+--model uniformer_base_IL \
 --img_size 32 128 128 \
 --crop_size 28 112 112 \
---eval-metric loss \
+--eval_metric loss \
 --lr 1e-4 \
 --min-lr 1e-6 \
 --warmup-epochs 5 \
 --epochs 200 \
---output output/7cls_test_resample_base/ \
+--output output/ \
 --mixup
 
